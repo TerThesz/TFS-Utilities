0e9fbf4360bf80dbb5f8192e28a822f5da16151b
TerThesz <60745997+TerThesz@users.noreply.github.com>
2020-11-03 19:06:07 +0100

    test beta

--- commands/mute.js
+++ commands/mute.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 module.exports.run = async (bot, message, args) => {
     let member = message.mentions.members.first();
     if(!message.member.hasPermission('MANAGE_MESSAGES') && !member.user.hasPermission('ADMINISTRATOR')) {
-        message.channel.send("Hana ňemožeš!");
+        message.channel.send("Haha ňemožeš!");
     } else {
         let mutedRole = member.guild.roles.cache.find(role => role.name === '↬ Mute');
         member.roles.add(mutedRole);
