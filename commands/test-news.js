8e2bbcdf363ec77c1316777a347abca731284f8e
TerThesz <60745997+TerThesz@users.noreply.github.com>
2020-11-03 16:17:31 +0100

    test beta

--- commands/test-news.js
+++ commands/test-news.js
@@ -4,6 +4,13 @@ module.exports.run = async (bot, message, args) => {
     if(!message.member.hasPermission('ADMINISTRATOR')) 
         message.channel.send("To si vyhoď z hlavy.");
     else {
+        const regex = new RegExp('"[^"]+"|[\\S]+', 'g');
+        const arguments = [];
+        args.match(regex).forEach(element => {
+            if (!element) return;
+            return arguments.push(element.replace(/"/g, ''));
+        });
+        console.log(arguments);
     }
 }
 
