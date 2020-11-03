db7092ba27e0351eb59c480a3212ed7c1bcc1076
TerThesz <60745997+TerThesz@users.noreply.github.com>
2020-11-03 16:51:06 +0100

    test beta

--- commands/test-news.js
+++ commands/test-news.js
@@ -10,7 +10,84 @@ module.exports.run = async (bot, message, args) => {
             if (!element) return;
             return arguments.push(element.replace(/"/g, ''));
         });
-        console.log(arguments);
+        switch(args.length) {
+            case 6:
+                const exampleEmbed = new Discord.MessageEmbed()
+                .setColor('#15902567')
+                .setTitle(args[0])
+                .setAuthor(message.sender.user.username, message.sender.user.avatar)
+                .setDescription(args[1])
+                .addFields(
+                    { name: args[2].split('|')[0], value: args[2].split('|')[1] },
+                    { name: args[3].split('|')[0], value: args[3].split('|')[1] },
+                    { name: args[4].split('|')[0], value: args[4].split('|')[1] },
+                    { name: args[5].split('|')[0], value: args[5].split('|')[1] },
+                )
+                .setTimestamp()
+                .setFooter('Novinky');
+                break;
+            case 5:
+                const exampleEmbed = new Discord.MessageEmbed()
+                .setColor('#15902567')
+                .setTitle(args[0])
+                .setAuthor(message.sender.user.username, message.sender.user.avatar)
+                .setDescription(args[1])
+                .addFields(
+                    { name: args[2].split('|')[0], value: args[2].split('|')[1] },
+                    { name: args[3].split('|')[0], value: args[3].split('|')[1] },
+                    { name: args[4].split('|')[0], value: args[4].split('|')[1] },
+                )
+                .setTimestamp()
+                .setFooter('Novinky');
+                break;
+            case 4:
+                const exampleEmbed = new Discord.MessageEmbed()
+                .setColor('#15902567')
+                .setTitle(args[0])
+                .setAuthor(message.sender.user.username, message.sender.user.avatar)
+                .setDescription(args[1])
+                .addFields(
+                    { name: args[2].split('|')[0], value: args[2].split('|')[1] },
+                    { name: args[3].split('|')[0], value: args[3].split('|')[1] },
+                )
+                .setTimestamp()
+                .setFooter('Novinky');
+                break;
+            case 3:
+                const exampleEmbed = new Discord.MessageEmbed()
+                .setColor('#15902567')
+                .setTitle(args[0])
+                .setAuthor(message.sender.user.username, message.sender.user.avatar)
+                .setDescription(args[1])
+                .addFields(
+                    { name: args[2].split('|')[0], value: args[2].split('|')[1] },
+                )
+                .setTimestamp()
+                .setFooter('Novinky');
+                break;
+            case 2:
+                const exampleEmbed = new Discord.MessageEmbed()
+                .setColor('#15902567')
+                .setTitle(args[0])
+                .setAuthor(message.sender.user.username, message.sender.user.avatar)
+                .setDescription(args[1])
+                .setTimestamp()
+                .setFooter('Novinky');
+                break;
+            case 1:
+                const exampleEmbed = new Discord.MessageEmbed()
+                .setColor('#15902567')
+                .setTitle(args[0])
+                .setAuthor(message.sender.user.username, message.sender.user.avatar)
+                .setTimestamp()
+                .setFooter('Novinky');
+                break;
+            default:
+                message.channel.send('Nepoužitelný počet argumentov. (očakávaný poČet argumentov 1-6)');
+                break;
+        }
+
+        channel.send(exampleEmbed);
     }
 }
 
