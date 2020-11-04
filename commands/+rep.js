12bbc387d45b7de1b60b923735b29087cb06c655
TerThesz <60745997+TerThesz@users.noreply.github.com>
2020-11-05 00:17:09 +0100

    test beta

--- commands/+rep.js
+++ commands/+rep.js
@@ -57,6 +57,10 @@ function checkRole(user, rep, message) {
     else if(rep >= 80 && rep <= 100) role = repRoles[3];
     else if(rep > 100) role = repRoles[2];
     var _role = message.guild.roles.cache.filter(role => role.id === role);
+    console.log(repRoles);
+    console.log(_role);
+    _role = message.guild.roles.cache.find(role => role.id === role);
+    console.log(_role);
     if (role) user.roles.add(_role);
     else console.log('prd');
 }
