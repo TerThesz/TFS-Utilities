065ef4d40901ad11c9de756bb3e548abeb810d4d
TerThesz <60745997+TerThesz@users.noreply.github.com>
2020-11-04 22:19:39 +0100

    test beta

--- commands/meme.js
+++ commands/meme.js
@@ -1,28 +1,27 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
-const randomPuppy = require('random-puppy');
-const snekfetch = require('snekfetch');
+  
+const Discord = require('discord.js');
+const got = require('got');
 
-module.exports.run = async (client, message, args) => {
-    const subReddits = ["memes", "meme", "me_irl", "dankmemes", "dankmeme", "pewdiepiesubmissions", "duklock", "madlad", "mildlyvandalised"]
-    const random = subReddits[Math.floor(Math.random() * subReddits.length)];
-
-    message.channel.startTyping();
-
-    randomPuppy(random).then(async url => {
-            await message.channel.send({
-                files: [{
-                    attachment: url,
-                    name: 'meme.png'
-                }]
-            }).then(() => message.channel.stopTyping());
-    }).catch(err => console.error(err));
+module.exports.run = async (bot, message, args) => {
+    const embed = new Discord.MessageEmbed();
+    got('https://www.reddit.com/r/memes/random/.json').then(response => {
+        let content = JSON.parse(response.body);
+        let permalink = content[0].data.children[0].data.permalink;
+        let memeUrl = `https://reddit.com${permalink}`;
+        let memeImage = content[0].data.children[0].data.url;
+        let memeTitle = content[0].data.children[0].data.title;
+        let memeUpvotes = content[0].data.children[0].data.ups;
+        let memeNumComments = content[0].data.children[0].data.num_comments;
+        embed.setTitle(`${memeTitle}`);
+        embed.setURL(`${memeUrl}`)
+        embed.setColor('RANDOM')
+        embed.setImage(memeImage);
+        embed.setFooter(`👍 ${memeUpvotes} 💬 ${memeNumComments}`);
+        message.channel.send(embed)
+    }).catch(console.error);
 }
 
 module.exports.config = {
     name: "meme",
-    description: "",
-    usage: "!meme",
-    accessableby: "Members",
     aliases: []
 }
