b7193645b944083454c1ecb4b003fa1095384192
TerThesz <60745997+TerThesz@users.noreply.github.com>
2020-11-04 22:12:54 +0100

    test beta

--- commands/meme.js
+++ commands/meme.js
@@ -1,17 +1,22 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
-const api = require("imageapi.js");
+const randomPuppy = require('random-puppy');
+const snekfetch = require('snekfetch');
 
 module.exports.run = async (client, message, args) => {
-    const subReddits = ["memes", "me_irl", "dankmemes", "pewdiepiesubmissions", "duklock", "madlad", "mildlyvandalised"]
+    const subReddits = ["memes", "meme", "me_irl", "dankmemes", "dankmeme", "pewdiepiesubmissions", "duklock", "madlad", "mildlyvandalised"]
     const random = subReddits[Math.floor(Math.random() * subReddits.length)];
-    let img = await api(random)
-    const Embed = new Discord.MessageEmbed()
-    .setTitle(`A meme from r/arabfunny`)
-    .setURL(`https://www.reddit.com/r/arabfunny`)
-    .setColor('RANDOM')
-    .setImage(img)
-    message.channel.send(Embed)
+
+    randomPuppy(random).then(url => {
+        snekfetch.get(url).then(async res => {
+            await message.channel.send({
+                files: ({
+                    attacment: res.body,
+                    name: "prd"
+                })
+            })
+        });
+    });
 }
 
 module.exports.config = {
