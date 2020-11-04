b89bba2ff09b0ece01f587c19eebe41dd6860361
TerThesz <60745997+TerThesz@users.noreply.github.com>
2020-11-04 21:47:40 +0100

    test beta

--- commands/meme.js
+++ commands/meme.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const snekfetch = require('snekfetch');
 
 module.exports.run = async (bot, message, args) => {
     const subReddits = ["memes", "me_irl", "dankmemes", "pewdiepiesubmissions", "duklock", "madlad", "mildlyvandalised"]
