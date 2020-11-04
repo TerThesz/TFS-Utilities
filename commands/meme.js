16c167918316d947676324701c94abebc530e15b
TerThesz <60745997+TerThesz@users.noreply.github.com>
2020-11-04 22:06:33 +0100

    test beta

--- commands/meme.js
+++ commands/meme.js
@@ -1,28 +1,17 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
-const snekfetch = require('snekfetch');
+const api = require("imageapi.js");
 
 module.exports.run = async (client, message, args) => {
     const subReddits = ["memes", "me_irl", "dankmemes", "pewdiepiesubmissions", "duklock", "madlad", "mildlyvandalised"]
     const random = subReddits[Math.floor(Math.random() * subReddits.length)];
-	try {
-        const { body } = await snekfetch
-            .get('https://www.reddit.com/r/' + random + '.json?sort=top&t=week')
-            .query({ limit: 10 });
-        const allowed = message.channel.nsfw ? body.data.children : body.data.children.filter(post => !post.data.over_18);
-        if (!allowed.length) return message.channel.send('It seems we are out of fresh memes!, Try again later.');
-        const randomnumber = Math.floor(Math.random() * allowed.length)
-        const embed = new Discord.RichEmbed()
-        .setColor(0x00A2E8)
-        .setTitle(allowed[randomnumber].data.title)
-        .setDescription("Posted by: " + allowed[randomnumber].data.author)
-        .setImage(allowed[randomnumber].data.url)
-        .addField("Other info:", "Up votes: " + allowed[randomnumber].data.ups + " / Comments: " + allowed[randomnumber].data.num_comments)
-        .setFooter("Memes provided by r/dankmemes")
-        message.channel.send(embed)
-    } catch (err) {
-        return console.log(err);
-    }
+    let img = await api(random)
+    const Embed = new Discord.MessageEmbed()
+    .setTitle(`A meme from r/arabfunny`)
+    .setURL(`https://www.reddit.com/r/arabfunny`)
+    .setColor('RANDOM')
+    .setImage(img)
+    msg.channel.send(Embed)
 }
 
 module.exports.config = {
