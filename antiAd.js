a5af9233ec7146aaf17edf96fa98444c2be797da
TerThesz <60745997+TerThesz@users.noreply.github.com>
2020-11-05 22:46:00 +0100

    test beta

--- antiAd.js
+++ antiAd.js
@@ -1,28 +1,30 @@
 module.exports = (client) => {
     const isInvite = async (guild, code) => {
-        return await new Promise(resolve =>{
-            guild.fetchInvites().then(invites => {
-                for (const invite in invites) {
-                    if (code === invite[0]) {
-                        resolve(true);
-                        return;
-                    }
-                }
-
-                resolve(false);
-                return;
-            }); 
-        });
+      return await new Promise((resolve) => {
+        guild.fetchInvites().then((invites) => {
+          for (const invite of invites) {
+            if (code === invite[0]) {
+              resolve(true)
+              return
+            }
+          }
+  
+          resolve(false)
+        })
+      })
     }
-
-    client.on('message', async message => {
-        const { member, content, guild } = message;
-
-        const code = content.split('discord.gg/')[1];
-
-        if (content.includes('discord.gg/')) {
-            const isGuildInv = await isInvite(guild, code)
-            console.log(isGuildInv);
-        }
+  
+    client.on('message', async (message) => {
+      const { guild, member, content } = message
+  
+      // discord.gg/23RAN4
+  
+      const code = content.split('discord.gg/')[1]
+      console.log('CODE:', code)
+  
+      if (content.includes('discord.gg/')) {
+        const isOurInvite = await isInvite(guild, code)
+        console.log(isOurInvite);
+      }
     })
-}
+  }
