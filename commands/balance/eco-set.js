48335189c70881d9eee67117adeb020e3af8efaf
TerThesz <60745997+TerThesz@users.noreply.github.com>
2020-11-08 18:47:56 +0100

    test beta

---
+++ commands/balance/eco-set.js
@@ -0,0 +1,55 @@
+const Discord = require("discord.js")
+const mongoose = require('mongoose');
+const { repRoles } = require('../../dataSets/bot.json');
+const talkedRecently = new Set();
+
+mongoose.connect(process.env.mongoose, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const Data = require('../../models/data.js');
+
+module.exports.run = async (bot, message, arguments) => {
+    arguments = message.content.split(' ').slice(1);
+
+    if (!message.member.hasPermission('ADMINISTRATOR')) return;
+    if (!arguments[1]) return;
+    if (arguments[1] != parseInt(arguments[1], 10)) return;
+    let user = message.mentions.members.first() || arguments[0];
+    if (user) {
+        Data.findOne({
+            userID: user.id
+        }, (err, data) => {
+            if(err) throw err;
+            if(!data) {
+                const newData = new Data({
+                    name: user.username,
+                    userID: user.id,
+                    rep: 0,
+                    messages: 0,
+                    balance: arguments[1],
+                    steamLinked: 'null',
+                    gamesPlayied: 0,
+                    pending: 'null',
+                });
+                data = newData;
+                newData.save().catch(err => console.log(err));
+                console.log('Created database table for ' + user.username);
+                message.channel.send('Rep set to ' + arguments[1]);
+            } else {
+                data.balance = arguments[1];
+                data.save().catch(err => console.log(err));
+                message.channel.send('Bal set to ' + arguments[1]);
+            }
+        })
+    } else {message.channel.send('Tohoto človeka nepoznám :(')}
+}
+
+module.exports.config = {
+    name: "eco-set",
+    description: "",
+    usage: "eco-set",
+    accessableby: "Admins",
+    aliases: []
+}
