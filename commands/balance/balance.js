f781a58b582e62da0161e8dfb9c7cc6168a21f37
TerThesz <60745997+TerThesz@users.noreply.github.com>
2020-11-08 16:54:41 +0100

    test beta

---
+++ commands/balance/balance.js
@@ -0,0 +1,47 @@
+const Discord = require("discord.js")
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.mongoose, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const Data = require('../../models/data.js');
+
+module.exports.run = async (bot, message, args) => {
+    var user = message.mentions.members.first() || message.author;
+    Data.findOne ({
+        userID = user.id
+    },(err, data) => {
+        if (err => console.log(err));
+
+        if(!data) {
+            const newData = new Data({
+                name: user.username,
+                userID: user.id,
+                rep: 0,
+                messages: 0,
+                balance: 0,
+                steamLinked: 'null',
+                gamesPlayied: 0,
+                pending: 'null',
+            });
+            newData.save().catch(err => console.log(err));
+            const exampleEmbed = new Discord.MessageEmbed()
+            .setColor('YELLOW')
+            .setDescription(`Používateľ \`${user.username}\` má na účte **0€**.`)
+            message.channel.send(exampleEmbed);
+        } else {
+            const exampleEmbed = new Discord.MessageEmbed()
+            .setColor('YELLOW')
+            .setDescription(`Používateľ \`${user.username}\` má na účte **${data.balance}€**.`)
+            message.channel.send(exampleEmbed);
+        }
+    });
+}
+
+module.exports.config = {
+    name: "banlance",
+    accessableby: "Members",
+    aliases: ['bal', 'cash', 'money', 'peniaze']
+}
