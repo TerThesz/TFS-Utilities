950eecabe5f827b6bf8caa14a9dd6ab4f1ad8004
TerThesz <60745997+TerThesz@users.noreply.github.com>
2020-11-30 22:18:42 +0100

    commit

--- customRooms.js
+++ customRooms.js
@@ -8,9 +8,9 @@ bot.on('voiceStateUpdate', (oldMember, newMember) => {
     newMember = bot.guilds.cache.find(guild => guild.id === '772916118930063360').members.cache.find(user => user.id === newMember);
 
     console.log('2');
+        console.log(newUserChannel + '\n' + newMember.user + '\n' + oldUserChannel + '\n' + oldMember.user);
         let newUserChannel = newMember.voice.channel;
         let oldUserChannel = oldMember.voice.channel;
-        console.log(newUserChannel + '\n' + newMember.user + '\n' + oldUserChannel + '\n' + oldMember.user);
         
         console.log('3');
         if(oldUserChannel === undefined && newUserChannel !== undefined) {
