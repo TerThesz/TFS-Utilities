430275846337730e53caa41e20a9fb895bf44039
TerThesz <60745997+TerThesz@users.noreply.github.com>
2020-11-30 21:59:04 +0100

    commit

--- customRooms.js
+++ customRooms.js
@@ -1,12 +1,17 @@
 const config = require('./dataSets/bot.json');
 
 module.exports = (bot) => {
+    console.log('1');
 bot.on('voiceStateUpdate', (oldMember, newMember) => {
+    console.log('2');
         let newUserChannel = newMember.voiceChannel
         let oldUserChannel = oldMember.voiceChannel
         
+        console.log('3');
         if(oldUserChannel === undefined && newUserChannel !== undefined) {
+            console.log('4');
             if (newUserChannel.id === config.customChannels.channel) {
+                console.log('5');
                 const channel = bot.guild.channels.cache.get(newMember.username);
                 if (channel) return newMember.send('Channel s tvojim menom už existuje.');
                 console.log('jj');
