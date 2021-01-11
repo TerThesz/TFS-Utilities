a7384921eed5d1426bd8002a802548e28f931ead
TerThesz <60745997+TerThesz@users.noreply.github.com>
2021-01-11 22:00:46 +0100

    commit

--- start.bat
+++ start.bat
@@ -1,5 +1,4 @@
 git add ./*
 git commit -m "commit"
 git branch -M main
-git remote add origin https://github.com/TerThesz/TFS-Utilities.git
-git push -u origin main
+git push https://github.com/TerThesz/TFS-Utilities.git
