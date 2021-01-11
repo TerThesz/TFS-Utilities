c063e9a2bb7cbf084d0d15bca4f1d1c7175d9edd
TerThesz <60745997+TerThesz@users.noreply.github.com>
2021-01-11 21:59:48 +0100

    commit

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 git add ./*
 git commit -m "commit"
 git branch -M main
-git remote add origin https://github.com/TerThesz/rtewt.git
+git remote add origin https://github.com/TerThesz/TFS-Utilities.git
 git push -u origin main
