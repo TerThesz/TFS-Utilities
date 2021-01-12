6a87d287167d556e64edf25e02f2c6aa92f36ff7
TerThesz <60745997+TerThesz@users.noreply.github.com>
2021-01-12 13:44:03 +0100

    commit

--- models/data.js
+++ models/data.js
@@ -1,28 +1,16 @@
 const mongoose = require('mongoose');
 
 const dataSchema = mongoose.Schema({
-    guildID: String,
-    guildName: String,
-    warns: Array,
-    config: Array,
-    shop: Array,
-    rules: Array,
-    blacklist: Array,
-    userData: {
-        default: {
-            name: String,
-            userID: String,
-            rep: Number,
-            messages: Number,
-            balance: Number,
-            steamLinked: String,
-            gamesPlayied: Number,
-            pending: String,
-            inventory: Array,
-            active: Array,
-        },
-        type: Array,
-    }
+    name: String,
+    userID: String,
+    rep: Number,
+    messages: Number,
+    balance: Number,
+    steamLinked: String,
+    gamesPlayied: Number,
+    pending: String,
+    inventory: Array,
+    active: Array,
 });
 
 module.exports = mongoose.model('Data', dataSchema);
